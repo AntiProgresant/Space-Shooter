dbd3dff5a07f3e200962ca59137666ce4ca61ce2
AntiProgresant <51537771+AntiProgresant@users.noreply.github.com>
2020-11-30 22:52:58 -0500

    First upload

---
+++ Space Shooter/Assets/Scripts/ThirdPresonCharacterController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPresonCharacterController : MonoBehaviour
+{
+    public float Speed;
+    public float maxSpeed = 6f;
+    public Rigidbody rb;
+    public float forwardForce = 200;
+   
+    void FixedUpdate()
+    {
+        PlayerMovement();
+
+    }
+
+    void PlayerMovement()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+        Vector3 playerMovement = new Vector3(hor, 0f, ver) * Speed * Time.deltaTime;
+        transform.Translate(playerMovement, Space.Self);
+    }
+}
